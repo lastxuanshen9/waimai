72855e1a02c61d6dd9f88295df82a0ca08b14671
lastxuanshen9 <72849493+lastxuanshen9@users.noreply.github.com>
2023-11-30 06:49:14 +0800

    外卖

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,15 +1,29 @@
 package com.sky.service;
 
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 public interface EmployeeService {
 
     /**
      * 员工登录
+     *
      * @param employeeLoginDTO
      * @return
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+    void save(EmployeeDTO employeeDTO);
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
 }
