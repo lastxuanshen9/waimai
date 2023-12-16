7e2d093fa17e163b8cf625e40a695ddf651a873c
lastxuanshen9 <72849493+lastxuanshen9@users.noreply.github.com>
2023-12-17 03:07:28 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -4,7 +4,9 @@ import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.DigestUtils;
 
@@ -20,10 +22,26 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
-    void save(EmployeeDTO employeeDTO);
-
     /**
      * 新增员工
      * @param employeeDTO
      */
+
+    void save(EmployeeDTO employeeDTO);
+
+    /**
+     * 分页查询
+     *
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, long id);
+
+
 }
